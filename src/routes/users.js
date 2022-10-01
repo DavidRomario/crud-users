628e4481bacc55674b6a71f4c41ae10aa3ac7fed
“David <romario.david1997@gmail.com>
2022-10-01 12:29:49 -0300

    update

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,7 +5,7 @@ const userController = require('../controllers/userController');
 // crud de um unico usuario, onde podemos: 1- listar as informaçoes dele, 2- criar usuario, 3- atualizar dados do usuario, 4- deletar usuario
 router.get('/:id', userController.getUserById);
 router.post('/create', userController.createUser);
-router.patch('/:id');
+router.patch("/:id", userController.updateUser);
 router.delete('/:id');
 
 module.exports = router;
