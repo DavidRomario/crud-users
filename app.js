10a4f36a46352414c5014e0354021bfd0338d517
David Romario <romario.david1997@gmail.com>
2022-10-26 14:04:02 -0300

    cors

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
 require("dotenv-safe").config();
 
-var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
-var productsRouter = require('./src/routes/products');
-var db = require('./src/config/database');
-
+var indexRouter = require("./src/routes/index");
+var usersRouter = require("./src/routes/users");
+var productsRouter = require("./src/routes/products");
+var db = require("./src/config/database");
 
 var app = express();
 db.connect();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 module.exports = app;
